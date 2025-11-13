99f690b54cd4084b3df783f3bb192d0515ba5bef
EIlif Johansen <eilifjohansen@outlook.com>
2025-11-13 02:26:31 +0100

    Flyttet til run.sh

--- run-dev.sh
+++ run-dev.sh
@@ -3,6 +3,9 @@
 # Enable Prisma debugging (optional - remove if you don't need it)
 export DEBUG="prisma:*:info"
 
+# Export DATABASE_URL with sslaccept parameter appended
+export DATABASE_URL="${NAIS_DATABASE_UMAMI_DEV_UMAMI_DEV_URL}&sslaccept=accept_invalid_certs"
+
 # Export REDIS_URL for the REDIS instance using the URI and credentials
 if [[ -n "$REDIS_USERNAME_UMAMI_DEV" && -n "$REDIS_PASSWORD_UMAMI_DEV" ]]; then
     export REDIS_URL="$(echo $REDIS_URI_UMAMI_DEV | sed "s#://#://$REDIS_USERNAME_UMAMI_DEV:$REDIS_PASSWORD_UMAMI_DEV@#")"
@@ -11,7 +14,7 @@ else
 fi
 
 # Debug output
-echo "DATABASE_URL: $(echo $NAIS_DATABASE_UMAMI_DEV_UMAMI_DEV_URL | sed 's/:[^:@]*@/:****@/')"
+echo "DATABASE_URL: $(echo $DATABASE_URL | sed 's/:[^:@]*@/:****@/')"
 echo "REDIS_URL: $(echo $REDIS_URL | sed 's/:[^:@]*@/:****@/')"
 
 # Start the application
