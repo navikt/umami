3374620f12ece9226fb6a0848356e3956b207747
Julian Nymark <julian.nymark@nav.no>
2026-06-08 17:38:30 +0200

    test changes with internal testing script: umami & sporing namespace

--- src/tracker/sporing-reops.js
+++ src/tracker/sporing-reops.js
@@ -48,8 +48,9 @@
 
   const domains = domain.split(",").map((n) => n.trim());
   const screen = `${width}x${height}`;
-  const eventRegex = /data-umami-event-([\w-_]+)/;
-  const eventNameAttribute = `${_data}umami-event`;
+  const eventRegex = /data-(?:sporing|umami)-event-([\w-_]+)/;
+  const eventNameAttribute = `${_data}sporing-event`;
+  const eventNameAttributeLegacy = `${_data}umami-event`;
   const delayDuration = 300;
 
   /* Helper functions */
@@ -110,7 +111,7 @@
 
   const handleClicks = () => {
     const trackElement = async (el) => {
-      const eventName = el.getAttribute(eventNameAttribute);
+      const eventName = el.getAttribute(eventNameAttribute) || el.getAttribute(eventNameAttributeLegacy);
       if (eventName) {
         const eventData = {};
 
@@ -128,7 +129,7 @@
       if (!parentElement) return trackElement(el);
 
       const { href, target } = parentElement;
-      if (!parentElement.getAttribute(eventNameAttribute)) return;
+      if (!parentElement.getAttribute(eventNameAttribute) && !parentElement.getAttribute(eventNameAttributeLegacy)) return;
 
       if (parentElement.tagName === "BUTTON") {
         return trackElement(parentElement);
@@ -156,6 +157,7 @@
   const trackingDisabled = () =>
     disabled ||
     !website ||
+    localStorage?.getItem("sporing.disabled") ||
     localStorage?.getItem("umami.disabled") ||
     (domain && !domains.includes(hostname)) ||
     (dnt && hasDoNotTrack());
@@ -233,11 +235,16 @@
 
   /* Start */
 
-  if (!window.umami) {
-    window.umami = {
+  if (!window.sporing) {
+    window.sporing = {
       track,
       identify,
     };
+  }
+
+  // Backwards compatibility
+  if (!window.umami) {
+    window.umami = window.sporing;
   }
 
   let currentUrl = normalize(href);
